2e2e379bedb2d0bdc0b09edf802e6236f8210b85
luoqpolyvi <luoq@polyvi.com>
2014-01-28 10:25:04 +0800

    Update JS snapshot to version 3.3.0-dev (via xsrc)

--- framework/assets/xface.js
+++ framework/assets/xface.js
@@ -1,5 +1,5 @@
 // Platform: android
-// 3.3.0-dev-80644fd
+// 3.3.0-dev-65b28c3
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var CORDOVA_JS_BUILD_LABEL = '3.3.0-dev-80644fd';
+var CORDOVA_JS_BUILD_LABEL = '3.3.0-dev-65b28c3';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
