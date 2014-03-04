72c3d67103cc483dc59a139568f861d1428a37ed
lis@polyvi.com <lis@polyvi.com>
2014-03-04 15:16:52 +0800

    xface.js can be loaded with single-file or discrete files

--- framework/src/com/polyvi/xface/util/XAssetsFileUtils.java
+++ framework/src/com/polyvi/xface/util/XAssetsFileUtils.java
@@ -1,4 +1,3 @@
-
 /*
  Copyright 2012-2013, Polyvi Inc. (http://polyvi.github.io/openxface)
  This program is distributed under the terms of the GNU General Public License.
@@ -17,125 +16,152 @@
 
  You should have received a copy of the GNU General Public License
  along with xFace.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package com.polyvi.xface.util;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import android.content.Context;
 
 public class XAssetsFileUtils {
-    private static final String CLASS_NAME = XAssetsFileUtils.class
-            .getSimpleName();
+	private static final String CLASS_NAME = XAssetsFileUtils.class
+			.getSimpleName();
 
-    /**
-     * 判断路径是否为目录
-     *
-     * @param context
-     * @param filePath
-     * @return
-     * @throws IOException
-     */
-    public static boolean isDirectory(Context context, String filePath) {
-        try {
-            context.getAssets().open(filePath);
-        } catch (IOException e) {
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * 判断路径是否为目录
+	 *
+	 * @param context
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean isDirectory(Context context, String filePath) {
+		if (!checkIfInAssets(context, filePath)) {
+			return false;
+		}
+		try {
+			context.getAssets().open(filePath);
+		} catch (IOException e) {
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * 判断路径是否为文件
-     *
-     * @param context
-     * @param filePath
-     * @return
-     * @throws IOException
-     */
-    public static boolean isFile(Context context, String filePath)
-            throws IOException {
-        try {
-            context.getAssets().open(filePath);
-        } catch (IOException e) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * 判断路径是否为文件
+	 *
+	 * @param context
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean isFile(Context context, String filePath) {
+		if (!checkIfInAssets(context, filePath)) {
+			return false;
+		}
+		try {
+			context.getAssets().open(filePath);
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * 递归的遍历assets目录
-     *
-     * @param context
-     *            上下文环境
-     * @param srcDir
-     *            需要遍历的目录路径
-     * @param visitor
-     *            文件处理器
-     */
-    public static void walkAssetsDirectory(Context context, String srcDir,
-            XFileVisitor visitor) {
-        try {
-            if (!isDirectory(context, srcDir) || !visitor.isContinueTraverse()) {
-                return;
-            }
-            String childrens[] = context.getAssets().list(srcDir);
-            for (int index = 0; index < childrens.length; index++) {
-                if (isFile(context, srcDir + File.separator + childrens[index])) {
-                    visitor.visit(srcDir + File.separator + childrens[index]);
-                } else {
-                    walkAssetsDirectory(context, srcDir + File.separator
-                            + childrens[index], visitor);
-                }
-            }
-        } catch (IOException e) {
-            XLog.e(CLASS_NAME, e.getMessage());
-            e.printStackTrace();
-            return;
-        }
-    }
+	/**
+	 * asset文件是否存在
+	 *
+	 * @param context
+	 * @param filePath
+	 * @return
+	 */
+	public static boolean checkIfInAssets(Context ctx, String assetName) {
+		if (assetName.startsWith(XConstant.ASSERT_PROTACAL)) {
+			assetName = assetName.substring(XConstant.ASSERT_PROTACAL.length());
+		}
+		List<String> mapList = null;
+		try {
+			mapList = Arrays.asList(ctx.getAssets().list(""));
+		} catch (IOException e) {
+			return false;
+		}
+		return mapList.contains(assetName) ? true : false;
+	}
 
-    /**
-     * @param context
-     *            系统上下文
-     * @param fileName
-     *            源文件名或者源目录名
-     * @param desPath
-     *            需要拷贝的目标路径
-     * @return 成功 true 失败 false
-     */
-    public static boolean copyAssetsToTarget(Context context,
-            String srcFileName, String desPath) {
-        try {
-            String childrens[] = context.getAssets().list(srcFileName);
-            if (childrens.length > 0) {
-                File desFile = new File(desPath);
-                if (!desFile.exists() && !desFile.mkdirs()) {
-                    throw new IOException();
-                }
-                int len = childrens.length;
-                for (int i = 0; i < len; i++) {
-                    copyAssetsToTarget(context, srcFileName + "/"
-                            + childrens[i],
-                            new File(desPath, childrens[i]).getAbsolutePath());
-                }
-            } else {
-                if (isFile(context, srcFileName)) {
-                    String targetFilePath = new File(desPath).getAbsolutePath();
-                    InputStream is = context.getAssets().open(srcFileName);
-                    XFileUtils.createFileByData(targetFilePath, is);
-                    is.close();
-                }
-            }
+	/**
+	 * 递归的遍历assets目录
+	 *
+	 * @param context
+	 *            上下文环境
+	 * @param srcDir
+	 *            需要遍历的目录路径
+	 * @param visitor
+	 *            文件处理器
+	 */
+	public static void walkAssetsDirectory(Context context, String srcDir,
+			XFileVisitor visitor) {
+		try {
+			if (!isDirectory(context, srcDir) || !visitor.isContinueTraverse()) {
+				return;
+			}
+			String childrens[] = context.getAssets().list(srcDir);
+			for (int index = 0; index < childrens.length; index++) {
+				if (isFile(context, srcDir + File.separator + childrens[index])) {
+					visitor.visit(srcDir + File.separator + childrens[index]);
+				} else {
+					walkAssetsDirectory(context, srcDir + File.separator
+							+ childrens[index], visitor);
+				}
+			}
+		} catch (IOException e) {
+			XLog.e(CLASS_NAME, e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+	}
 
-        } catch (IOException e) {
-            XLog.e(CLASS_NAME, e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * @param context
+	 *            系统上下文
+	 * @param fileName
+	 *            源文件名或者源目录名
+	 * @param desPath
+	 *            需要拷贝的目标路径
+	 * @return 成功 true 失败 false
+	 */
+	public static boolean copyAssetsToTarget(Context context,
+			String srcFileName, String desPath) {
+		try {
+			String childrens[] = context.getAssets().list(srcFileName);
+			if (childrens.length > 0) {
+				File desFile = new File(desPath);
+				if (!desFile.exists() && !desFile.mkdirs()) {
+					throw new IOException();
+				}
+				int len = childrens.length;
+				for (int i = 0; i < len; i++) {
+					copyAssetsToTarget(context, srcFileName + "/"
+							+ childrens[i],
+							new File(desPath, childrens[i]).getAbsolutePath());
+				}
+			} else {
+				if (isFile(context, srcFileName)) {
+					String targetFilePath = new File(desPath).getAbsolutePath();
+					InputStream is = context.getAssets().open(srcFileName);
+					XFileUtils.createFileByData(targetFilePath, is);
+					is.close();
+				}
+			}
+
+		} catch (IOException e) {
+			XLog.e(CLASS_NAME, e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
